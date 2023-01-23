81d06474bf3332531686349b47e1ab9d01efddad
niino <niinomateus@gmail.com>
2023-01-23 00:48:11 -0300

    Solving looping and onclick problems

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
-let contador = 0
 let clickCounter = 0
 const boxList = document.querySelectorAll('.box')
 
 
-while(contador < 9){
+for(let contador = 0; contador < boxList.length; contador ++){
     const boxElement = boxList[contador];
-
-    //pq N TA FUNCIONANDO ESSA FUNCAO?P
     boxElement.onclick = function(){
-        alert()
-    }
-
-    console.log(clickCounter);
-
-    if(clickCounter == 0 || clickCounter == 2 || clickCounter == 4 || clickCounter == 6 || clickCounter == 8){
-        boxElement.onclick = function(){
-            boxElement.classList.add('X')
+        if(clickCounter == 0 || clickCounter == 2 || clickCounter == 4 || clickCounter == 6 || clickCounter == 8){
+            boxElement.classList.add('X');   
+            clickCounter ++;
+            console.log(clickCounter);
+        } else {
+            boxElement.classList.add('O');
+            clickCounter++;
+            console.log(clickCounter);
         }
     }
-
-    if(clickCounter == 1 || clickCounter == 3 || clickCounter == 5 || clickCounter == 7 || clickCounter == 9){
-        console.log(boxElement);
-        boxElement.onclick = function(){
-            boxElement.classList.add('Y')
-        }
-    }
-
-    contador += 1;
+    console.log(clickCounter)
 }
